1319ab9ac6281620e60c7b3c6a34885ffb0a0718
Arafat-Forhaz <arafat69forhaz@gmail.com>
2024-10-10 10:20:26 +0600

    updated frontend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 morgan.token('body', request => JSON.stringify(request.body))
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan("tiny"))
 
